7f45da910d1710a6951ff7eb93483a759d46a6bc
2dof <36607325+2dof@users.noreply.github.com>
2023-05-19 13:01:28 +0200

    Add files via upload

--- process_models/simple_models_esp.py
+++ process_models/simple_models_esp.py
@@ -1,36 +1,9 @@
 #MIT License
 #Copyright (c) 2023 Lukasz Szydlowski,  
-## simple_models.py - discrere models of LTI process  
+## simple_models_esp.py - discrere models of LTI process  
  
 import array
-
-class ring_buffer(object):
-    def __init__(self, bufferSize =5):
-        
-        self.bufferSize = bufferSize 
-        self.data     =  array.array('f', [0]*bufferSize)
-        self.it  = 0    # index of tail position 
-        self.ndata = 0  # No of data
-            
-    def add_data(self,data): 
-        
-          if self.ndata>=self.bufferSize:
-              self.ndata-=1 # overflow
-        
-          self.data[self.ii] = data
-          self.ndata+=1
-          self.ii = (self.ii+1) % self.bufferSize 
-   
-    def get_data(self):
-        
-        if self.ndata ==0 :
-            return []
-            
-        x = self.data[self.it] 
-        self.ndata-=1
-        self.it = (self.it+1) % self.bufferSize   # 
-
-        return x
+from RingBuffer import ring_buffer
 
 def discrete_FOP(y_1,Ts,u,Kp,taup):
     yk=  taup/(taup+Ts)*y_1 + Kp*Ts/(taup+Ts) * u
@@ -48,7 +21,7 @@ class FOPDT_model(object):
         self.ndelay = int(self.delay/self.Ts)
         self.buf    = ring_buffer(bufferSize =self.ndelay+1)
         self.buf.ii = self.ndelay
-        
+     
     def update(self,u):
         
         self.buf.add_data(u)    
@@ -73,21 +46,18 @@ class dc_motor():
         self._x1 = 0.     # dot(phi) [rad/s]
         self._x2 = 0.     # i: current [A]
         
-    
     def update(self, V,Td=0,Ts=0.01):
         
         self._x0 = self._x0 + Ts* self._x1 
         self._x1 = self._x1 + Ts/self.J*(self.Kt*self._x2 - self.bm*self._x1 - Td)
         self._x2 = self._x2 + Ts/self.L*(V - self.R*self._x2 - self.Kb*self._x1)
-        
         self._x0 -= (self._x0//6.2832) * 6.2832
         return self._x1   
 
 
-
 if __name__ == '__main__':
     
-    model = dc_motor() #  FOPDT_model()
+    model =  FOPDT_model()  #dc_motor() # 
     print('model step responce')
     for i in range(0,100):
         yk = model.update(1.0)
